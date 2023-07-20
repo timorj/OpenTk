2051a0fa25845dff2874ad99786603adbb0815a2
lipengfei <handsomelpf@outlook.com>
2023-07-20 20:52:16 +0800

    test3

--- OpenTKtest/Program.cs
+++ OpenTKtest/Program.cs
@@ -19,16 +19,9 @@ namespace OpenTKtest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             using (Window game = new Window(800, 600, "LearnOpenTK"))
-                game.Run(60.0); game.Run(60.0);
-            game.Run(60.0); game.Run(60.0);
-                //Run takes a double, which is how many frames per second it should strive to reach.
-                //Run takes a double, which is how many frames per second it should strive to reach.
-                //You can leave that out and it'll just update as fast as the hardware will allow it.
-                //You can leave that out and it'll just update as fast as the hardware will allow it.
-                //You can leave that out and it'll just update as fast as the hardware will allow it.
-                game.Run(60.0);game.Run(60.0);
-            }
+                dfasdfasdf
+            }sdfadfa
 
-        }
+        dfadf}
     }
-}
+}dfadf
